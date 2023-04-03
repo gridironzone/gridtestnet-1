7adb88767bdd5dc769bdb5677f95ce2ddfb2da65
phunkatrunc <phunkatrunc@gmail.com>
2023-04-04 03:53:34 +0530

    commit

--- all_data.sh
+++ all_data.sh
@@ -13,12 +13,6 @@ sudo apt install git build-essential ufw curl jq snapd wget --yes
 wget -q -O - https://git.io/vQhTU | bash -s -- --version 1.19.1
 source /home/adrian/.bashrc
 
-# Note: install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Note: Add Homebrew to bin
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/adrian/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Note: Download and install the Gridiron Zone Binary
 git clone https://github.com/furynet/furyhub -b genesis 
