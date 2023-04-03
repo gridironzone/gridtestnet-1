858e04cbcb1a5e903180f1aaca3fa04e4ed227e6
phunkatrunc <phunkatrunc@gmail.com>
2023-04-04 03:04:21 +0530

    genesis

--- all_data.sh
+++ all_data.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-PASSWORD="FuryT3sTN#T"
+PASSWORD="F@nfuryG#n3sis@fury"
 
-fury init local --chain-id pandora-4
-
-echo "Backing up existing genesis file..."
-cp "$HOME"/.fury/config/genesis.json "$HOME"/.fury/config/genesis.json.backup
-
-echo "Copying new genesis file to $HOME/.fury/config/genesis.json..."
-cp genesis.json "$HOME"/.fury/config/genesis.json
+fury init local --chain-id gridiron_4200-1001
 
 yes $PASSWORD | fury keys add genArgentina
 yes $PASSWORD | fury keys add genBrazil
@@ -19,13 +13,17 @@ yes $PASSWORD | fury keys add genNY-Yankees
 yes $PASSWORD | fury keys add genSF-Giants
 
 # Note: important to add 'genBrazil' as a genesis-account since this is the chain's validator
-yes $PASSWORD | fury add-genesis-account "$(fury keys show genArgentina -a)" 1000000000000ufury,1000000000000ufusd
-yes $PASSWORD | fury add-genesis-account "$(fury keys show genBrazil -a)" 1000000000000ufury
-yes $PASSWORD | fury add-genesis-account "$(fury keys show genBrooklyn-Nets -a)" 1000000000000ufury
+yes $PASSWORD | fury add-genesis-account "$(fury keys show genArgentina -a)" 120000020000utfury
+yes $PASSWORD | fury add-genesis-account "$(fury keys show genBrazil -a)" 30537820000utfury
+yes $PASSWORD | fury add-genesis-account "$(fury keys show genBrooklyn-Nets -a)" 25000000000utfury
+yes $PASSWORD | fury add-genesis-account "$(fury keys show genIndia-Football -a)" 200000000000utfury
+yes $PASSWORD | fury add-genesis-account "$(fury keys show genLA-Lakers -a)" 132572320000utfury
+yes $PASSWORD | fury add-genesis-account "$(fury keys show genNY-Yankees -a)" 200066960000utfury
+yes $PASSWORD | fury add-genesis-account "$(fury keys show genSF-Giants -a)" 23264580000utfury
 
 
 # Set staking token (both bond_denom and mint_denom)
-sed -i -e 's/stake/ufury/g' /home/adrian/.fury/config/genesis.json
+sed -i -e 's/stake/utfury/g' /home/adrian/.fury/config/genesis.json
 
 
 # Set reserved bond tokens
@@ -40,12 +38,12 @@ TO="minimum-gas-prices = \"0.025$FEE_TOKEN\""
 sed -i -e 's/$FROM/$TO/g' "$HOME"/.fury/config/app.toml
 
 # TODO: config missing from new version (REF: https://github.com/cosmos/cosmos-sdk/issues/8529)
-#fury config chain-id pandora-4
+#fury config chain-id gridiron_4200-1001
 #fury config output json
 #fury config indent true
 #fury config trust-node true
 
-fury gentx genBrazil 1000000ufury --chain-id pandora-4
+fury gentx genBrazil 1000000utfury --chain-id gridiron_4200-1001
 
 fury collect-gentxs
 fury validate-genesis
@@ -69,4 +67,5 @@ sed -i -e '107s/$FROM/$TO/g' "$HOME"/.fury/config/app.toml
 sed -i -e 's/timeout_commit = "5s"/timeout_commit = "1s"/g' "$HOME"/.fury/config/config.toml
 sed -i -e 's/timeout_propose = "3s"/timeout_propose = "1s"/g' "$HOME"/.fury/config/config.toml
 
-fury start --pruning "nothing"
+# fury start --pruning "nothing"
+echo "You're good to go!!"
