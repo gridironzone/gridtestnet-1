390c81b248b7c835ec988f4f3b7da0456a1e1b53
phunkatrunc <phunkatrunc@gmail.com>
2023-04-05 04:35:57 +0530

    commit

--- all_data-node3.sh
+++ all_data-node3.sh
@@ -1,20 +1,68 @@
-#!/usr/bin/env bash
+#Setting up constants
+FURY_HOME=$HOME/.fury
+FURY_SRC=$FURY_HOME/src/fury
+COSMOVISOR_SRC=$FURY_HOME/src/cosmovisor
 
-# Note: Download updates to the VM
-sudo apt update
-sudo apt upgrade
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt install git build-essential ufw curl jq snapd wget --yes
+FURY_VERSION="v1.0.1"
+COSMOVISOR_VERSION="cosmovisor-v1.0.1"
 
-# Note: Download go@1.19.1
-wget -q -O - https://git.io/vQhTU | bash -s -- --version 1.19.1
-(echo; echo 'eval "$(/home/.go)"') >> /home/adrian/.profile
+mkdir -p $FURY_HOME
+mkdir -p $FURY_HOME/src
+mkdir -p $FURY_HOME/bin
+mkdir -p $FURY_HOME/logs
+mkdir -p $FURY_HOME/cosmovisor/genesis/bin
+mkdir -p $FURY_HOME/cosmovisor/upgrades/
 
-# Note: Download Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/adrian/.profile
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "-----------installing dependencies---------------"
+sudo dnf -y update
+sudo dnf -y copr enable ngompa/musl-libc
+sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo dnf -y install subscription-manager
+sudo subscription-manager config --rhsm.manage_repos=1
+sudo subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
+sudo dnf makecache --refresh
+sudo dnf -y --skip-broken install curl nano ca-certificates tar git jq gcc-c++ gcc-toolset-9 openssl-devel musl-devel musl-gcc gmp-devel perl python3 moreutils wget nodejs make hostname procps-ng pass libsecret pinentry crudini cmake
+
+gcc_source="/opt/rh/gcc-toolset-9/enable"
+if test -f $gcc_source; then
+   source gcc_source
+fi
+
+set -eu
+
+echo "--------------installing golang---------------------------"
+curl https://dl.google.com/go/go1.16.4.linux-amd64.tar.gz --output $HOME/go.tar.gz
+tar -C $HOME -xzf $HOME/go.tar.gz
+rm $HOME/go.tar.gz
+export PATH=$PATH:$HOME/go/bin
+export GOPATH=$HOME/go
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+go version
+
+echo "----------------------installing fury---------------"
+git clone -b furyhub-1 https://github.com/fanfury-sports/fanfury.git $FURY_SRC
+cd $FURY_SRC
+make build
+mv fury $FURY_HOME/cosmovisor/genesis/bin/fury
+
+echo "-------------------installing cosmovisor-----------------------"
+git clone -b $COSMOVISOR_VERSION https://github.com/onomyprotocol/onomy-sdk $COSMOVISOR_SRC
+cd $COSMOVISOR_SRC
+make cosmovisor
+cp cosmovisor/cosmovisor $FURY_HOME/bin/cosmovisor
+
+echo "-------------------adding binaries to path-----------------------"
+chmod +x $FURY_HOME/bin/*
+export PATH=$PATH:$FURY_HOME/bin
+chmod +x $FURY_HOME/cosmovisor/genesis/bin/*
+export PATH=$PATH:$FURY_HOME/cosmovisor/genesis/bin
+
+echo "export PATH=$PATH" >> ~/.bashrc
+
+# set the cosmovisor environments
+echo "export DAEMON_HOME=$FURY_HOME/" >> ~/.bashrc
+echo "export DAEMON_NAME=fury" >> ~/.bashrc
+echo "export DAEMON_RESTART_AFTER_UPGRADE=true" >> ~/.bashrc
 
 
 # Note: Download the keys files
@@ -22,13 +70,6 @@ git clone https://github.com/gridironzone/gridtestnet-1
 cd gridtestnet-1/testnet-1
 mv keys ~/
 cd 
-
-# Note: Download and install the Gridiron Binary
-git clone https://github.com/fanfury-sports/fanfury -b fanfury
-cd fanfury
-make install
-cd ..
-
 rm -rf ~/.fury
 
 PASSWORD="F@nfuryG#n3sis@fury"
@@ -43,7 +84,7 @@ fury init gridiron_4200-3 --chain-id $CHAIN_ID --staking-bond-denom utfury
 curl -o ~/.fury/config/genesis.json https://raw.githubusercontent.com/fanfury-sports/download-1/main/testnet-1/genesis.json
 
 # Note: Add an account
-yes $PASSWORD | fury keys import node2 ~/keys/node2.key
+yes $PASSWORD | fury keys import node3 ~/keys/node3.key
 
 
 # Set staking token (both bond_denom and mint_denom)
@@ -55,7 +96,7 @@ FROM="\"mint_denom\": \"stake\""
 TO="\"mint_denom\": \"$STAKING_TOKEN\""
 sed -i -e "s/$FROM/$TO/" "$HOME"/.fury/config/genesis.json
 
-# Set node3 token (both for gov min deposit and crisis constant node3)
+# Set fury token (both for gov min deposit and crisis constant fury)
 FEE_TOKEN="utfury"
 FROM="\"stake\""
 TO="\"$FEE_TOKEN\""
@@ -67,7 +108,7 @@ FROM="\"reserved_bond_tokens\": \[\]"
 TO="\"reserved_bond_tokens\": \[$RESERVED_BOND_TOKENS\]"
 sed -i -e "s/$FROM/$TO/" "$HOME"/.fury/config/genesis.json
 
-# Set min-gas-prices (using node3 token)
+# Set min-gas-prices (using fury token)
 FROM="minimum-gas-prices = \"\""
 TO="minimum-gas-prices = \"0.000002$FEE_TOKEN\""
 sed -i -e "s/$FROM/$TO/" "$HOME"/.fury/config/app.toml
@@ -77,7 +118,7 @@ FROM="\"voting_period\": \"172800s\""
 TO="\"voting_period\": \"$MAX_VOTING_PERIOD\""
 sed -i -e "s/$FROM/$TO/" "$HOME"/.fury/config/genesis.json
 
-yes $PASSWORD | fury gentx node2 1000000utfury --chain-id $CHAIN_ID
+yes $PASSWORD | fury gentx node3 1000000utfury --chain-id $CHAIN_ID
 fury collect-gentxs
 fury validate-genesis
 
