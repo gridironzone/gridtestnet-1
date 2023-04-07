6964fc00a5f2268de2f2dd3b2aed6c96a77f1789
phunkatrunc <phunkatrunc@gmail.com>
2023-04-07 12:14:55 +0530

    commit

--- testnet-1/setup_binary.sh
+++ testnet-1/setup_binary.sh
@@ -59,27 +59,27 @@ fury collect-gentxs
 # ---------------------------------------------------------------------------- #
 #                              Set configurations                              #
 # ---------------------------------------------------------------------------- #
-sed -i -e "/\[rpc\]/,+3 s/laddr *= .*/laddr = \"tcp:\/\/$SETTLEMENT_ADDR\"/" "$TENDERMINT_CONFIG_FILE"
-sed -i -e "/\[p2p\]/,+3 s/laddr *= .*/laddr = \"tcp:\/\/$P2P_ADDRESS\"/" "$TENDERMINT_CONFIG_FILE"
-sed  -i '' -e "s/^persistent_peers =.*/persistent_peers = \"$HUB_PEERS\"/" "$TENDERMINT_CONFIG_FILE"
+sed -i -e '/\[rpc\]/,+3 s/laddr *= .*/laddr = \"tcp:\/\/$SETTLEMENT_ADDR\"/' '$TENDERMINT_CONFIG_FILE'
+sed -i -e '/\[p2p\]/,+3 s/laddr *= .*/laddr = \"tcp:\/\/$P2P_ADDRESS\"/' '$TENDERMINT_CONFIG_FILE'
+sed  -i -e 's/^persistent_peers =.*/persistent_peers = \"$HUB_PEERS\"/' '$TENDERMINT_CONFIG_FILE'
 
-sed -i -e "/\[grpc\]/,+6 s/address *= .*/address = \"$GRPC_ADDRESS\"/" "$APP_CONFIG_FILE"
-sed -i -e "/\[grpc-web\]/,+7 s/address *= .*/address = \"$GRPC_WEB_ADDRESS\"/" "$APP_CONFIG_FILE"
-sed -i -e "s/^chain-id *= .*/chain-id = \"$CHAIN_ID\"/" "$CLIENT_CONFIG_FILE"
+sed -i -e '/\[grpc\]/,+6 s/address *= .*/address = \"$GRPC_ADDRESS\"/' '$APP_CONFIG_FILE'
+sed -i -e '/\[grpc-web\]/,+7 s/address *= .*/address = \"$GRPC_WEB_ADDRESS\"/' '$APP_CONFIG_FILE'
+sed -i -e 's/^chain-id *= .*/chain-id = \"$CHAIN_ID\"/' "$CLIENT_CONFIG_FILE"
 
-sed -i -e "s/^node *= .*/node = \"tcp:\/\/$SETTLEMENT_ADDR\"/" "$CLIENT_CONFIG_FILE"
+sed -i -e 's/^node *= .*/node = \"tcp:\/\/$SETTLEMENT_ADDR\"/' "$CLIENT_CONFIG_FILE"
 sed -i -e 's/bond_denom": ".*"/bond_denom": "ufury"/' "$GENESIS_FILE"
 sed -i -e 's/mint_denom": ".*"/mint_denom": "ufury"/' "$GENESIS_FILE"
 
-sed -i -e 's/^minimum-gas-prices *= .*/minimum-gas-prices = "0ufury"/' "$APP_CONFIG_FILE"
-sed -i -e '/\[api\]/,+3 s/enable *= .*/enable = true/' "$APP_CONFIG_FILE"
-sed -i -e "/\[api\]/,+9 s/address *= .*/address = \"tcp:\/\/$API_ADDRESS\"/" "$APP_CONFIG_FILE"
+sed -i -e 's/^minimum-gas-prices *= .*/minimum-gas-prices = "0ufury"/' '$APP_CONFIG_FILE'
+sed -i -e '/\[api\]/,+3 s/enable *= .*/enable = true/' '$APP_CONFIG_FILE'
+sed -i -e '/\[api\]/,+9 s/address *= .*/address = \"tcp:\/\/$API_ADDRESS\"/' '$APP_CONFIG_FILE'
 
 if [ -n "$UNSAFE_CORS" ]; then
   echo "Setting CORS"
-  sed -ie 's/enabled-unsafe-cors.*$/enabled-unsafe-cors = true/' "$APP_CONFIG_FILE"
-  sed -ie 's/enable-unsafe-cors.*$/enabled-unsafe-cors = true/' "$APP_CONFIG_FILE"
-  sed -ie 's/cors_allowed_origins.*$/cors_allowed_origins = ["*"]/' "$TENDERMINT_CONFIG_FILE"
+  sed -ie 's/enabled-unsafe-cors.*$/enabled-unsafe-cors = true/' '$APP_CONFIG_FILE'
+  sed -ie 's/enable-unsafe-cors.*$/enabled-unsafe-cors = true/' '$APP_CONFIG_FILE'
+  sed -ie 's/cors_allowed_origins.*$/cors_allowed_origins = ["*"]/' '$TENDERMINT_CONFIG_FILE'
 fi
 
 
